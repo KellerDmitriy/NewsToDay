545d9403a60ee5315d57b63daaab0e6861371113
DmKeller <dmit.keller@gmail.com>
2024-03-17 19:00:55 +0300

    Initial Commit

---
+++ NewsToDay/NewsToDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsToDayApp.swift
+//  NewsToDay
+//
+//  Created by Келлер Дмитрий on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsToDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
