66341f722b07b339233698f7c70f5a0cd2468e82
Me <rubikone89pw@rambler.ru>
2024-03-18 22:14:45 +0300

    a custom tabbar has been created

--- NewsToDay/Screens/TabBar/CustomTabBar.swift
+++ NewsToDay/Screens/TabBar/CustomTabBar.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct CustomTabBar: View {
+    
+    @Binding var tabSelection: Int
+    @Namespace private var animationNamespace
+    
+    let tabBarItems: [(image: String, title: String)] = [
+    ("house", "Home"),
+    ("squareshape.split.2x2", "Categories"),
+    ("bookmark", "Bookmarks"),
+    ("person", "Profile"),
+    ]
+    
     var body: some View {
 
-        Text("Курнул, ну, вроде не навоз")
-
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height: 80)
+                .foregroundColor(Color(.secondarySystemBackground))
+                .shadow(radius: 2)
+                .opacity(0.8)
+            
+            HStack {
+                ForEach(0..<4) { index in
+                    Button {
+                       tabSelection = index + 1
+                    } label: {
+                        VStack(spacing: 8) {
+                            Spacer()
+                            
+                            Image(systemName: tabBarItems[index].image)
+                                .padding(28)
+                                .offset(y: -10)
+                                
+//                            Text(tabBarItems[index].title)
+//                                .offset(y: -10)
+                        }
+                        .foregroundColor(index + 1 == tabSelection ? .blue : .green)
+                    }
+                    
+                }
+            }
+            .frame(height: 80)
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    CustomTabBar()
+    CustomTabBar(tabSelection: .constant(1))
+        .previewLayout(.sizeThatFits)
+        .padding(.vertical)
 }
