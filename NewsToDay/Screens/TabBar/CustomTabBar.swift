b73d863fdcfcec70b8bac0403ea881a9787c80ec
Me <rubikone89pw@rambler.ru>
2024-03-18 20:51:52 +0300

    created a branch for customTabBar

---
+++ NewsToDay/Screens/TabBar/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  NewsToDay
+//
+//  Created by Максим Нурутдинов on 18.03.2024.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text("Курнул, ну, вроде не навоз")
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
