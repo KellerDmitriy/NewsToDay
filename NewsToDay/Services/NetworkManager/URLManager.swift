b2b50744c42d9c76b7861a7a81658f0a28e2eace
DmKeller <dmit.keller@gmail.com>
2024-03-18 23:07:14 +0300

    fix

--- NewsToDay/Services/NetworkManager/URLManager.swift
+++ NewsToDay/Services/NetworkManager/URLManager.swift
@@ -11,14 +11,22 @@ final class URLManager {
     static let shared = URLManager()
     private init() {}
     
+    #warning("APIKEY")
+    private let apiKey = ["2b9cf27ea13e45eb89926c533fb14c6b"]
+        
     private let baseURL = "https://newsapi.org"
     private let apiEndpoint = "/api"
     
     func createURL(with path: String, queryItems: [URLQueryItem] = []) -> URL? {
-        var urlComponents = URLComponents(string: baseURL)
+        var urlComponents = URLComponents()
+        urlComponents?.scheme = "https"
+        urlComponents.host = "newsapi.org"
         urlComponents?.path = apiEndpoint + path
         urlComponents?.queryItems = queryItems
         
+        urlComponents.scheme = endpoint.scheme
+        urlComponents.host = endpoint.host
+        urlComponents.path = endpoint.path
         return urlComponents?.url
     }
 }
