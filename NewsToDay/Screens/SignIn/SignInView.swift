1f27b724e61a17b2636e414c43e8d094f33136c7
Me <rubikone89pw@rambler.ru>
2024-03-23 16:17:38 +0300

    refactoring and corrections

--- NewsToDay/Screens/SignIn/SignInView.swift
+++ NewsToDay/Screens/SignIn/SignInView.swift
@@ -22,38 +22,46 @@ struct SignInView : View {
             
             
             Text("I am happy to see you again. You can continue where you left off by logging in")
-                .foregroundColor(Color.gray.opacity(0.5))
+                .foregroundColor(Color(hex: "#7C82A1"))
                 .padding([.top, .leading], 10)
             VStack (alignment: .leading) {
-                
                 VStack(alignment: .leading) {
-                    
                     VStack(alignment: .leading) {
-
-                        
                         HStack {
+                            Image(systemName: "envelope")
+                                .foregroundColor(.gray)
+                                .padding(.leading, 10)
+                                .padding(.trailing, 5)
                             
-                            TextField("Email Adress", text: $user).padding()
-                                .frame(width: 336, height: 56)
-                                .background(Color.gray)
-                                .clipShape(Rectangle()).cornerRadius(10)
+                            Spacer()
+                            
+                            TextField("Email Adress", text: $user)
+                                .padding()
+                                .frame(height: 56)
+                                .background(Color(hex: "#F3F4F6"))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
                             
                             if user != ""{
-                                
                                 Image("check").foregroundColor(Color.init(.label))
                             }
-                            
                         }
-                        
                     }.padding(.bottom, 15)
                     
                     VStack(alignment: .leading) {
-                        
-                        SecureField("Password", text: $pass).padding()
-                            .frame(width: 336, height: 56)
-                            .background(Color.gray)
-                            .clipShape(Rectangle()).cornerRadius(10)
-                        
+                        HStack {
+                            Image(systemName: "lock.rectangle")
+                                .foregroundColor(.gray)
+                                .padding(.leading, 10)
+                                .padding(.trailing, 5)
+                            
+                            Spacer()
+                            
+                            SecureField("Password", text: $pass)
+                                .padding()
+                                .frame(height: 56)
+                                .background(Color(hex: "#F3F4F6"))
+                                .clipShape(Rectangle()).cornerRadius(10)
+                        }
                     }
                     
                 }.padding(.horizontal, 6)
@@ -69,29 +77,24 @@ struct SignInView : View {
                     Text("Sign In")
                         .foregroundColor(.white)
                         .frame(width: UIScreen.main.bounds.width - 120).padding()
-                        .background(Color.blue)
-                    
-                    
-                    
-                }.background(Color("color"))
-                    .clipShape(Rectangle()).cornerRadius(10)
-                    .padding(.top, 45)
-                
+                        .background(Color(hex: "#475AD7"))
+                }
+                .background(Color("color"))
+                .clipShape(Rectangle()).cornerRadius(10)
+                .padding(.top, 45)
                 
                 Spacer()
                 
                 HStack(spacing: 8){
-                    
-                    Text("Don't Have An Account ?").foregroundColor(Color.gray.opacity(0.5))
-                    
+                    Text("Don't Have An Account ?")
+                        .foregroundColor(Color(hex: "#333647"))
+                        
                     Button(action: {
-                        
+        
                     }) {
-                        
                         Text("Sign Up")
-                        
-                    }.foregroundColor(.black)
-                    
+                    }
+                    .foregroundColor(.black)
                 }.padding(.top, 25)
             }
         }
